e28e31c39bea53c2d2413396d56076b3e8c6a0df
Gustavo <96484053+gmayaute@users.noreply.github.com>
2024-12-23 22:35:04 -0500

    Add files via upload

---
+++ Controllers/ProductosController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductoNetCore.Models;
+
+namespace ProductoNetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductosController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/productos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Producto>>> GetProductos()
+        {
+            return await _context.Productos.ToListAsync();
+        }
+
+        // POST: api/productos
+        [HttpPost]
+        public async Task<ActionResult<Producto>> PostProducto(Producto producto)
+        {
+            _context.Productos.Add(producto);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetProductos), new { id = producto.Id }, producto);
+        }
+    }
+}
