bdc1402b726162c13837457869b699f3dd45ea4c
Gustavo <96484053+gmayaute@users.noreply.github.com>
2024-12-23 22:36:35 -0500

    Add files via upload

---
+++ Models/ApplicationDBContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductoNetCore.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
